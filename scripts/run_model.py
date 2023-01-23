d28306cc53e4778645d39f2b837d78d9cf5829d3
hienhuynhdang <hienhuynh406@icloud.com>
2023-01-23 23:34:58 +0700

    sync to db

--- scripts/run_model.py
+++ scripts/run_model.py
@@ -5,4 +5,5 @@ import pickle
 
 if __name__ == "__main__":
     model = pickle.load(open('src/ml_models/rain_forest.model', 'rb'))
-    print(model)
+    result = model.predict([[0.7,11.0,12.9,3.2,57.7,4.2,170.0,0.2,8,20.784],[0.7,1.0,2.9,3.2,57.7,4.2,170.0,0.2,8,20.784]])
+    print(type(result))
---
+++ scripts/sync_to_db.py
@@ -0,0 +1,3 @@
+# --- Open cmt line bellow if run by cmd: python *.py
+import sys  # nopep8
+sys.path.append(".")  # nopep8
